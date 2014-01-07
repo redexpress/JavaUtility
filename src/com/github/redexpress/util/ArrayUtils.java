0b7ea8b2192a1d6ac47751e30fc7846ed16ba6c3
Gavin Yang <redppl@hotmail.com>
2014-01-07 11:44:53 +0800

    add array like python

---
+++ src/com/github/redexpress/util/ArrayUtils.java
@@ -0,0 +1,21 @@
+package com.github.redexpress.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayUtils {
+	public static List<Integer> range(int from, int to, int step){
+		List<Integer> l = new ArrayList<Integer>();
+		//TODO
+		return l;
+	}
+	public static List<Integer> range(int from, int to){
+		return range(from, to, 1);
+	}
+	public static List<Integer> range(int to){
+		return range(0, to, 1);
+	}
+	public static void main(String[] args) {
+		
+	}
+}
